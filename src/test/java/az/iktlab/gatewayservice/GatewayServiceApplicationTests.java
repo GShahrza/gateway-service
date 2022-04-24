55216d46310b97ca41609f9c9735833558a50d9b
Shahrza <gshahrza@gmail.com>
2022-04-24 23:06:39 +0400

    gateway service created!

---
+++ src/test/java/az/iktlab/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package az.iktlab.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
