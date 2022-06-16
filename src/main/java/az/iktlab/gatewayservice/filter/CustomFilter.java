b457be41bc4ec07bb475283874b5eb802752b6dc
Shahrza Gahramanov <gshahrza@gmail.com>
2022-06-16 21:29:35 +0400

    new

---
+++ src/main/java/az/iktlab/gatewayservice/filter/CustomFilter.java
@@ -0,0 +1,36 @@
+package az.iktlab.gatewayservice.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class CustomFilter implements GlobalFilter {
+
+    @Autowired
+    private WebClient.Builder builder;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if (exchange.getRequest().getPath().toString().contains("au th")) {
+            return chain.filter(exchange);
+        } else {
+            String jwt;
+            try {
+                jwt = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            } catch (NullPointerException e) {
+                throw new NullPointerException("Token not found");
+            }
+            return builder.build().get()
+                    .uri("http://localhost:8080/auth/token")
+                    .header("Authorization", jwt)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .map(s -> exchange)
+                    .flatMap(chain::filter);
+        }
+    }
+}
